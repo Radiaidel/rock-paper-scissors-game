18947345d8bbb95144364362d9d9cc1133b42823
unknown <zinebmac49@gmail.com>
2023-10-26 16:52:29 +0100

    starting js

---
+++ script.js
@@ -0,0 +1,26 @@
+const pHands = [
+    {
+        "id":3,
+        "src": "images/paper1.png",
+        "class": "paper",
+        "alt": "palyer-paper"
+    },
+    {
+        "id":4,
+        "src": "images/rock1.png",
+        "class": "rock",
+        "alt": "palyer-rock"
+    },
+    {
+        "id":5,
+        "src": "images/scissor1.png",
+        "class": "sci",
+        "alt": "palyer-scissor"
+    },
+];
+let scoreP = 0;
+let scoreR = 0;
+let name = document.getElementById("pseudo-name");
+
+const hand1 = document.querySelector(".finger1");
+hand1.appendChild()
